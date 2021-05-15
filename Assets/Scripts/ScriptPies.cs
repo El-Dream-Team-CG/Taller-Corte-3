d8734131ff416197e73518712521ac0fbfb2484a
pipejimenez100 <pipejimenez100@gmail.com>
2021-05-14 19:43:39 -0500

    Initial commit

---
+++ Assets/Scripts/ScriptPies.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptPies : MonoBehaviour
+{
+    public animacionPersonaje1 animacionpersonaje;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        animacionpersonaje.puedoSaltar = true;
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        animacionpersonaje.puedoSaltar = false;
+
+    }
+}
