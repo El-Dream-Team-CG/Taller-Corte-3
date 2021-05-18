6acfe33ad8062f6a983086600a82f1adcbfb89c3
Ains0peg <juan_jose.melendez@uao.edu.co>
2021-05-18 13:10:57 -0500

    Inicio Scena 2
    Holi

---
+++ Assets/Scripts/LogicaPJ2.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LogicaPJ2 : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float velocidadM;
+    public float velocidadR;
+    private Animator animacion;
+    public float x, y;
+
+    //salto
+    public float fuerzaDeSalto = 8f;
+    public bool puedoSaltar;
+
+    void Start()
+    {
+        puedoSaltar = false;
+        velocidadM = 5f;
+        velocidadR = 200f;
+        animacion = GetComponent<Animator>();
+    }
+
+    void FixedUpdate()
+    {
+        transform.Rotate(0, x * Time.deltaTime * velocidadR, 0);
+        transform.Translate(0, 0, y * Time.deltaTime * velocidadM);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        x = Input.GetAxis("Horizontal");
+        y = Input.GetAxis("Vertical");
+
+        animacion.SetFloat("VelocidadX", x);
+        animacion.SetFloat("VelocidadY", y);
+
+        if (puedoSaltar)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                animacion.SetBool("Salte", true);
+                rb.AddForce(new Vector3(0, fuerzaDeSalto, 0), ForceMode.Impulse);
+            }
+            animacion.SetBool("TocoSuelo", true);
+        }
+        else
+        {
+            EstoyCayendo();
+        }
+    }
+
+    public void EstoyCayendo()
+    {
+        animacion.SetBool("TocoSuelo", false);
+        animacion.SetBool("Salte", false);
+    }
+}
---
+++ Assets/Scripts/LogicaPies.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LogicaPies : MonoBehaviour
+{
+    public LogicaPJ2 logicaPJ2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        logicaPJ2.puedoSaltar = true;
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        logicaPJ2.puedoSaltar = false;
+
+    }
+}
