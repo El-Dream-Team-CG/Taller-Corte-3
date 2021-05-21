c5660cb01dfa6eb56b73fa34165aafe538b9ee4e
Ains0peg <juan_jose.melendez@uao.edu.co>
2021-05-21 15:44:42 -0500

    hola
    final

---
+++ Assets/Scripts/plataformasMundo.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class plataformasMundo : MonoBehaviour
+{
+    // public float speed2= 1.0f;
+    public Vector3 dir;
+    public float speed;
+    public float PosicionInferior;
+    public float PosicionSuperior;
+    // Start is called before the first frame update
+    void Start()
+    {
+        dir = Vector3.up;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       transform.Translate(dir * speed * Time.deltaTime, Space.World);
+
+        if (transform.position.y >= PosicionSuperior)
+        {
+            dir = Vector3.down;
+        }
+
+        if (transform.position.y <= PosicionInferior)
+        {
+            dir = Vector3.up;
+
+        }
+    }
+}
+
---
+++ Assets/Scripts/teleport.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class teleport : MonoBehaviour
+{
+    public Transform teleportTarget;
+    public GameObject LogicaPJ2;
+
+    void OnTriggerEnter(Collider other)
+    {
+        LogicaPJ2.transform.position = teleportTarget.transform.position;
+    }
+}
